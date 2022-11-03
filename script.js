77bc4bd2b0585cfa718046e640361b2e0972e613
Michael Perea <mperea104@cnm.edu>
2022-11-03 13:07:04 -0600

    updated website for checkin

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
-let wrapper = document.querySelectorAll('.wrapper');
-let loadMoreBtn = document.querySelector('#loadMore');
-let currentRow = 2
-loadMoreBtn.addEventListener('click', function() {
-    for (let i = currentRow; i < currentRow + 2; i++) {
-        if(wrapper[i]){
-           wrapper[i].style.display = 'block';
-        }
-    }
-    currentRow += 2;
-    if(currentRowItems >= wrapper.length){
-        event.target.style.display = 'none';
-    }
-})
+//An Array uses square brackets [] but an Object uses curly braces {}
+let loadMore = () => {
+    let boxes = document.getElementsByClassName("d-none");
+    console.log(boxes)
+    // let boxesArray = Object.values(boxes)
+    // console.log(boxesArray[0])
+    boxes[0].classList.remove("d-none");
+}
